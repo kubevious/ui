1e6a9133754ccf899cf5f35a291e7d9c51f9566c
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-01-13 10:22:15 -0800

    injected version

--- src/lib/routers/web.js
+++ src/lib/routers/web.js
@@ -3,5 +3,11 @@ module.exports = ({app}) => {
     app.get('/', function(req, res) {
         res.render('index');
     });
+
+    app.get('/version', function (req, res) {
+        res.send({
+            version: require('../../version')
+        });
+    });
     
 };
