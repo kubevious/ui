522ca20eaedb0544e757b286b46b76c38cc640b9
uuuulala <kondrashova.ksenya@gmail.com>
2020-02-14 14:38:02 -0600

    history chart adjustments (2 axis, refresh, SVG scaling)

--- playground/history-chart-control/js/history-chart.js
+++ playground/history-chart-control/js/history-chart.js
@@ -1,57 +1,39 @@
 function applyHistoryChartData(data) {
-    const margin = { top: 50, right: 50, bottom: 90, left: 75 };
-    const width = 500;
-    const height = 200;
+    const width = document.querySelector("#chart").clientWidth;
+    const height = document.querySelector("#chart").clientHeight;
+    const margin = { top: height * 0.2, right: width * 0.1, bottom: height * 0.2, left: width * 0.1 };
 
     // Create SVG element
     const svg = d3
-        .select("#chart")
-        .append("svg")
+        .select("#chart svg");
+
+    svg
+        .html("")
         .attr("viewBox", (-margin.left) + " " + (-margin.top) + " " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom));
 
     // Set up scales
     const bisectDate = d3.bisector(function(d) {
         return +Date.parse(d.date);
     }).left;
-    const maxY = Math.max(
-        d3.max(data, function(d) {
-            return d.items;
-        }),
-        d3.max(data, function(d) {
-            return d.alerts;
-        })
-    );
     const xScale = d3
         .scaleTime()
         .domain(d3.extent(data, function(d) {
             return +Date.parse(d.date);
         }))
         .range([0, width]);
-    const yScale = d3
+    const yScaleLeft = d3
         .scaleLinear()
-        .domain([0, maxY])
+        .domain(d3.extent(data, function(d) { return d.items; }))
         .range([height, 0]);
-
-    // Append axis to the chart
-    svg
-        .append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(d3
-            .axisBottom(xScale)
-            .tickFormat(function(d){
-                return d3.timeFormat("%H:%M")(d)
-            })
-        );
-    svg
-        .append("g")
-        .attr("class", "y axis")
-        .call(d3.axisLeft(yScale));
+    const yScaleRight = d3
+        .scaleLinear()
+        .domain(d3.extent(data, function(d) { return d.alerts; }))
+        .range([height, 0]);
 
     // Create & append line chart
     const line = d3.line()
         .x(function(d) { return xScale(+Date.parse(d.date)); })
-        .y(function(d) { return yScale(d.items);  });
+        .y(function(d) { return yScaleLeft(d.items);  });
     svg.append("path")
         .datum(data)
         .attr("class", "line")
@@ -60,8 +42,8 @@ function applyHistoryChartData(data) {
     // Create & append area chart
     const area = d3.area()
         .x(function(d) { return xScale(new Date(d.date)); })
-        .y0(yScale(0))
-        .y1(function(d) { return yScale(d.alerts); });
+        .y0(height)
+        .y1(function(d) { return yScaleRight(d.alerts); });
     svg.append("path")
         .datum(data)
         .attr("class", "area")
@@ -77,6 +59,28 @@ function applyHistoryChartData(data) {
     selector
         .append("path")
         .attr("d", "M0," + (-0.3 * margin.top) + " v" + (height + margin.top));
+
+
+    // Append axis to the chart
+    svg
+        .append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0, " + height + ")")
+        .call(d3
+            .axisBottom(xScale)
+            .tickFormat(function(d){
+                return d3.timeFormat("%H:%M")(d)
+            })
+        );
+    svg
+        .append("g")
+        .attr("class", "y axis")
+        .call(d3.axisLeft(yScaleLeft));
+    svg
+        .append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + width + ", 0)")
+        .call(d3.axisRight(yScaleRight));
 
     // Create a transparent rect over the chart to handle mouse events
     svg
