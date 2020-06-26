4e280d45e106bd93cfbd4d8c927121cd10858c13
Semyon Pukhov <semyonpuchof@gmail.com>
2020-06-26 11:43:11 +0300

    Changes for loading indicator and error box

--- src/src/utils/remote-track.js
+++ src/src/utils/remote-track.js
@@ -1,27 +1,52 @@
+import { v4 as uuidv4 } from 'uuid';
+import { isEmptyObject } from './util';
+
 class RemoteTrack {
     constructor(sharedState) {
         this._sharedState = sharedState
+        this._requests = {}
     }
 
-    start({ action }) {
+    start({ action, options }) {
         this._sharedState.set('is_loading', true)
+
+        const request = {
+            id: uuidv4(),
+            options,
+            complete: false,
+        }
+
+        this._requests[request.id] = request
+
         console.log(`[REMOTE_TRACK] => ${action}`)
+
+        return {
+            request: request.id,
+
+            complete: () => {
+                this._requests[request.id].complete = true
+                this.detectLoading()
+            },
+
+            fail: (error) => {
+                this._sharedState.set('is_error', true)
+                this._sharedState.set('error', error)
+
+                this.detectLoading()
+            },
+        }
     }
 
-    complete() {
-        this._sharedState.set('is_loading', false)
-    }
+    detectLoading() {
+        for (let key in this._requests) {
+            if (this._requests[key].complete !== false) {
+                delete this._requests[key]
+            }
+        }
 
-    fail(error) {
-        this._sharedState.set('is_error', true)
-        this._sharedState.set('error', error)
-
-        setTimeout(() => {
-            this._sharedState.set('is_error', false)
-            this._sharedState.set('error', null)
-        }, 4000)
-
-        this._sharedState.set('is_loading', false)
+        if (isEmptyObject(this._requests)) {
+            this._sharedState.set('is_loading', false)
+        }
     }
 }
 
