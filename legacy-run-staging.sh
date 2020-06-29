e5616998fb0753356b6639a088f5cda7f0bde807
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-06-29 12:28:53 -0700

    more cleanup

--- legacy-run-staging.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
-MY_DIR="$(dirname $MY_PATH)"
-cd $MY_DIR
-
-export NODE_ENV=production
-export BACKEND_URL=http://localhost:4000
-node src-legacy/.
