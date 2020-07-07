d8650cbc69aa66ded6a50e8429f57d11b0561dc2
Semyon Pukhov <semyonpuchof@gmail.com>
2020-07-07 10:35:03 +0300

    Moved components registration into separate file

---
+++ src/src/components/GoldenLayout/register-components.js
@@ -0,0 +1,60 @@
+import Diagram from '../Diagram';
+import RuleEditor from '../Editors/RuleEditor';
+import MarkerEditor from '../Editors/MarkerEditor';
+import Properties from '../Properties';
+import Alerts from '../Alerts';
+import Timeline from '../Timeline';
+
+class RegisterComponents {
+    constructor(parent) {
+        this._parent = parent
+        
+        this.setupComponents()
+    }
+
+    setupComponents() {
+        this._parent._register({
+            name: 'Universe',
+            component: Diagram,
+            location: 'main',
+            title: 'Universe',
+            skipClose: true
+        })
+        this._parent._register({
+            name: 'Rule Editor',
+            component: RuleEditor,
+            location: 'main',
+            title: 'Rule Editor'
+        })
+        this._parent._register({
+            name: 'Marker Editor',
+            component: MarkerEditor,
+            location: 'main',
+            title: 'Marker Editor'
+        })
+        this._parent._register({
+            name: 'Properties',
+            component: Properties,
+            location: 'right',
+            title: 'Properties',
+            width: 25,
+            allowVerticalScroll: true
+        })
+        this._parent._register({
+            name: 'Alerts',
+            component: Alerts,
+            location: 'bottom',
+            title: 'Alerts',
+            allowVerticalScroll: true
+        })
+        this._parent._register({
+            name: 'Timeline',
+            component: Timeline,
+            location: 'bottom',
+            title: 'Timeline',
+            allowVerticalScroll: true
+        })
+    }
+}
+
+export default RegisterComponents
