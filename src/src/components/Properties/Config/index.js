8d59fdf7ce9710964ccfeba50ce673b17a1fe5a3
Semyon Pukhov <semyonpuchof@gmail.com>
2020-07-27 15:29:10 +0300

    Updated config popup, implement read-only mode

--- src/src/components/Properties/Config/index.js
+++ src/src/components/Properties/Config/index.js
@@ -1,27 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import jsyaml from 'js-yaml'
 import hljs from 'highlight.js'
+import DnComponent from '../../DnComponent'
+import { faDownload, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { parseDn } from '../../../utils/naming-utils';
+import cx from 'classnames'
 
 import './styles.scss'
-import DnComponent from '../../DnComponent'
+
 
 const Config = ({ group, dn }) => {
-    const code = jsyaml.safeDump(group.config)
+    const [indent, setIndent] = useState(2)
+
+    const code = jsyaml.safeDump(group.config, { indent })
 
     const renderCode = () => {
         const result = hljs.highlight(group.kind, code)
 
         return (
             <pre>
-                <code dangerouslySetInnerHTML={{ __html: result.value }}/>
+                <code dangerouslySetInnerHTML={{ __html: result.value }} />
             </pre>
         )
     }
+
+    const downloadFile = () => {
+        const namespace = parseDn(dn).find(item => item.kind === 'ns').name
+        const fileName = namespace ? `${namespace}.yaml` : `config.yaml`
+
+        const blob = new Blob([code], { type: 'application/yaml'})
+        const exportElem = document.getElementById('exportAnchor')
+        exportElem.setAttribute('href', window.URL.createObjectURL(blob))
+        exportElem.setAttribute('download', fileName)
+        exportElem.click()
+    }
+
     return (
         <>
-            {dn && <div className="container-header">
-                <DnComponent dn={dn}/>
-                <h3>Config</h3>
+            {dn && <div className="Config-header">
+                <div className="cluster">
+                    <DnComponent dn={dn} />
+                </div>
+
+                <div className="header">
+                    <a id='exportAnchor' style={{ display: 'none' }} />
+                    <h3>Config</h3>
+                    <div className="buttons-group">
+                        <span className="tab-label">
+                            Tab Size
+                        </span>
+
+                        <button
+                            className={cx('config-btn', { 'selected': indent === 2 })}
+                            onClick={() => setIndent(2)}
+                        >
+                            2
+                        </button>
+                        <button
+                            className={cx('config-btn mr-25', { 'selected': indent === 4 })}
+                            onClick={() => setIndent(4)}
+                        >
+                            4
+                        </button>
+
+                        <button className="config-btn mr-25"><FontAwesomeIcon icon={faPencilAlt} /></button>
+
+                        <button
+                            className="config-btn download mr-25"
+                            onClick={() => downloadFile()}
+                        >
+                            <FontAwesomeIcon icon={faDownload} />
+                        </button>
+                    </div>
+                </div>
             </div>}
             <div className="Config-container">
                 {renderCode()}
