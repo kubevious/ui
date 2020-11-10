693f65e3d2eda15b63cd83bde9b34c33b8aa97ed
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-11-10 11:22:31 -0800

    new version mock changes.

--- src/src/boot/aboutMockData.js
+++ src/src/boot/aboutMockData.js
@@ -14,23 +14,31 @@ export const ABOUT_DATA = [
     },
 ];
 
-export const NEW_VERSION_CONTENT = `
+const MD_CONTENT = `
+You can easily navigate to certain time and watch cluster condition
+
+![Kubevious Time Machine screen](https://github.com/kubevious/media/raw/master/screens/time-machine-1.png)
+
+### Scheduled updates
+
+| Product           | Dec   | Jan   | Feb   |
+|-------------------|-------|-------|-------|
+| Kubevious UI      | 4.5.7 | 4.5.8 | 4.5.9 |
+| Kubevious backend |       | 1.2.4 |       |
+| Collector         |       |       | 8.8.9 |
+
 ### Other improvements
 
 - Added personal message notification to user
 - Added notification snooze functionality
 - Made improvements to the UI/UX
-
-### Planned in new versions
-
-- Add advanced search filtering
 `
 
 export const NEW_VERSION_AVAILABLE_DATA = {
     "kind": "new-version",
     "name": "Kubevious",
     "version": "1.6.1",
-    "url": "https://github.com/kubevious/kubevious",
+    "url": "https://github.com/kubevious/kubevious/releases/tag/v0.6",
     "changes": [
         "Rule editor. Define custom rules to continuously validate Kubernetes cluster and apps configurations.",
         "Marker editor. Associate custom markers to configurations using smart filters.",
@@ -46,7 +54,7 @@ export const NEW_VERSION_AVAILABLE_DATA = {
         "Markers editor",
         "Diagram autorefresh"
     ],
-    "content": NEW_VERSION_CONTENT
+    "content": MD_CONTENT
 }
 
 export const FEEDBACK_QUESTIONS = {
@@ -78,23 +86,11 @@ export const FEEDBACK_QUESTIONS = {
     ]
 }
 
-const CONTENT = `
-You can easily navigate to certain time and watch cluster condition
 
-![Kubevious Time Machine screen](https://github.com/kubevious/media/raw/master/screens/time-machine-1.png)
-
-
-### Scheduled updates
-
-| Product           | Dec   | Jan   | Feb   |
-|-------------------|-------|-------|-------|
-| Kubevious UI      | 4.5.7 | 4.5.8 | 4.5.9 |
-| Kubevious backend |       | 1.2.4 |       |
-| Collector         |       |       | 8.8.9 |`
 
 export const MESSAGE_DATA = {
     "kind": "message",
     "id": "123456-e88e",
     "title": "Try out moving back in time with our Timeline function!",
-    "content": CONTENT
+    "content": MD_CONTENT
 }
