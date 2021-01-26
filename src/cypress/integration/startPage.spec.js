8ae647b0c83bee17fdcc037e8fe792e4638bab65
Semyon Pukhov <semyonpuchof@gmail.com>
2021-01-26 14:54:16 +0300

    Test fix

--- src/cypress/integration/startPage.spec.js
+++ src/cypress/integration/startPage.spec.js
@@ -2,7 +2,7 @@ describe('Open application', () => {
     it('Open summary', () => {
         cy.visit('/')
 
-        cy.get('#summary').should('be.visible')
+        cy.get('#summaryComponent').should('be.visible')
         cy.get('#diagram').should('not.be.visible')
         cy.get('#ruleEditorComponent').should('not.be.visible')
         cy.get('#markerEditorComponent').should('not.be.visible')
@@ -16,15 +16,15 @@ describe('Open application', () => {
         })
 
         cy.contains('Universe').click()
-        cy.get('#summary').should('not.be.visible')
+        cy.get('#summaryComponent').should('not.be.visible')
         cy.get('#diagram').should('be.visible')
 
         cy.contains('Rule Editor').click()
-        cy.get('#summary').should('not.be.visible')
+        cy.get('#summaryComponent').should('not.be.visible')
         cy.get('#ruleEditorComponent').should('be.visible')
 
         cy.contains('Marker Editor').click()
-        cy.get('#summary').should('not.be.visible')
+        cy.get('#summaryComponent').should('not.be.visible')
         cy.get('#markerEditorComponent').should('be.visible')
     })
 })
