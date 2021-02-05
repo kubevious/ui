15258dfdc94ddd371f9c0fbae6f34822bc674ec8
Semyon Pukhov <semyonpuchof@gmail.com>
2021-02-05 17:05:07 +0300

    Bringing kubevious-handler to .ts

--- src/src/state/kubevious-handler.ts
+++ src/src/state/kubevious-handler.ts
@@ -1,14 +1,18 @@
 import StateHandler from './state-handler'
 import DiagramSource from './diagram-source'
+import RootApiService from '../services/RootApiService';
 
 class KubeviousHandler
 {
-    constructor(api, info)
+    _api: RootApiService;
+    _stateHandler: StateHandler;
+    _diagramSource: DiagramSource;
+
+    constructor(api: RootApiService)
     {
         this._api = api;
-        this._info = info;
 
-        console.log("[KubeviousHandler] :: create", this._info);
+        console.log("[KubeviousHandler] :: create");
 
         this._stateHandler = new StateHandler(
             api.sharedState,
@@ -16,12 +20,12 @@ class KubeviousHandler
 
         this._diagramSource = new DiagramSource(
             api.sharedState,
-            api.socketService(info));
+            api.socketService());
     }
 
-    close()
+    close(): void
     {
-        console.log("[KubeviousHandler] :: destroy", this._info);
+        console.log("[KubeviousHandler] :: destroy");
 
         this._stateHandler.close();
         this._diagramSource.close();
