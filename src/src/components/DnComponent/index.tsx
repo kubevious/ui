4dd2d754cb36a59fba7fadf02472ab7aaa6b0e92
Semyon Pukhov <semyonpuchof@gmail.com>
2021-02-11 16:49:54 +0300

    Update DnComponent

--- src/src/components/DnComponent/index.tsx
+++ src/src/components/DnComponent/index.tsx
@@ -1,45 +1,46 @@
 import React, { Fragment } from 'react'
 import _ from 'the-lodash'
 import { getNodeLogoUrl, prettyKind } from '../../utils/ui-utils'
-import { DnOptions } from './types';
+import { DnOptions } from './types'
 
 import * as DnUtils from '@kubevious/helpers/dist/dn-utils'
 
 import './styles.scss'
 
-export const DnComponent = ({ dn, options }: {dn: string, options?: DnOptions}): JSX.Element => {
-    const opt = options || {};
+type DnComponentProps = {
+    dn: string
+    options?: DnOptions
+}
+
+export const DnComponent: React.FunctionComponent<DnComponentProps> = ({ dn, options }) => {
+    const opt = options || {}
 
     let dnParts = DnUtils.parseDn(dn)
-    const lastPart = _.last(dnParts);
+    const lastPart = _.last(dnParts)
 
-    if (opt.relativeTo)
-    {
-        var parentDnParts = DnUtils.parseDn(opt.relativeTo);
-        while((dnParts.length > 0) && 
-            (parentDnParts.length > 0) && 
-            (dnParts[0].rn === parentDnParts[0].rn))
-        {
-            dnParts.shift();
-            parentDnParts.shift();
+    if (opt.relativeTo) {
+        var parentDnParts = DnUtils.parseDn(opt.relativeTo)
+        while (dnParts.length > 0 && parentDnParts.length > 0 && dnParts[0].rn === parentDnParts[0].rn) {
+            dnParts.shift()
+            parentDnParts.shift()
         }
     }
 
     if (dnParts.length > 0) {
         if (dnParts[0].kind === 'root') {
-            dnParts = dnParts.splice(1);
+            dnParts = dnParts.splice(1)
         }
     }
 
     return (
-        <div className="dn-path">
-            <img className="dn-logo" src={getNodeLogoUrl(lastPart.kind)} alt="logo" />
+        <div className='dn-path'>
+            <img className='dn-logo' src={getNodeLogoUrl(lastPart.kind)} alt='logo' />
             {dnParts.map((item, index) => (
                 <Fragment key={index}>
-                    <span className="kind">{prettyKind(item.kind)}</span>
-                    <span className="name">{item.name}</span>
-                    {index !== dnParts.length - 1 && <span className="separator">&gt;</span>}
-                    {index === dnParts.length - 1 && <div className="clearfix"/>}
+                    <span className='kind'>{prettyKind(item.kind)}</span>
+                    <span className='name'>{item.name}</span>
+                    {index !== dnParts.length - 1 && <span className='separator'>&gt;</span>}
+                    {index === dnParts.length - 1 && <div className='clearfix' />}
                 </Fragment>
             ))}
         </div>
