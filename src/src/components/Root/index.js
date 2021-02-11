4d1f57507af762a40284fcd2f8867ad9c50d7b3d
Semyon Pukhov <semyonpuchof@gmail.com>
2021-02-11 17:29:02 +0300

    Code formating

--- src/src/components/Root/index.js
+++ src/src/components/Root/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import './styles.scss'
-import GoldenLayoutComponent from '../GoldenLayout'
-import Popup from '../Popup'
-import Header from '../Header'
-import BaseComponent from '../../HOC/BaseComponent'
-import SEO from '../SEO'
-import FieldsSaver from '../../utils/save-fields'
-import ErrorBox from '../ErrorBox';
+import React from "react"
+import "./styles.scss"
+import GoldenLayoutComponent from "../GoldenLayout"
+import Popup from "../Popup"
+import Header from "../Header"
+import BaseComponent from "../../HOC/BaseComponent"
+import SEO from "../SEO"
+import FieldsSaver from "../../utils/save-fields"
+import ErrorBox from "../ErrorBox"
 
 class Root extends BaseComponent {
     constructor(props) {
-        super(props);
+        super(props)
 
         this.state = {
             showPopup: false,
@@ -18,107 +18,118 @@ class Root extends BaseComponent {
             layout: null,
             windows: [],
             isError: false,
-            error: null
+            error: null,
         }
 
-        this._fieldsSaver = new FieldsSaver('Diagram')
+        this._fieldsSaver = new FieldsSaver("Diagram")
 
         this.handleLayout = this.handleLayout.bind(this)
         this.handleChangeWindow = this.handleChangeWindow.bind(this)
         this.closeError = this.closeError.bind(this)
 
-        this.subscribeToSharedState([
-            'selected_dn',
-            'time_machine_enabled', 
-            'time_machine_target_date',
-            'time_machine_date_to',
-            'time_machine_duration'
-        ],
+        this.subscribeToSharedState(
+            [
+                "selected_dn",
+                "time_machine_enabled",
+                "time_machine_target_date",
+                "time_machine_date_to",
+                "time_machine_duration",
+            ],
             ({
                 selected_dn,
-                time_machine_enabled, 
-                time_machine_target_date, 
+                time_machine_enabled,
+                time_machine_target_date,
                 time_machine_date_to,
-                time_machine_duration                
+                time_machine_duration,
             }) => {
-
                 this._fieldsSaver.setValue({
                     selected_dn,
-                    time_machine_enabled, 
-                    time_machine_target_date, 
+                    time_machine_enabled,
+                    time_machine_target_date,
                     time_machine_date_to,
-                    time_machine_duration 
+                    time_machine_duration,
                 })
-            })
+            }
+        )
     }
 
     handleLayout(value) {
         this.setState({
-            layout: value, windows: value._components
-                .filter(item => !item.skipClose)
-                .map(component => ({ ...component, isVisible: true }))
+            layout: value,
+            windows: value._components
+                .filter((item) => !item.skipClose)
+                .map((component) => ({ ...component, isVisible: true })),
         })
 
-        this.subscribeToSharedState(['selected_dn', 'auto_pan_to_selected_dn'], 
-            ({selected_dn, auto_pan_to_selected_dn}) => {
+        this.subscribeToSharedState(
+            ["selected_dn", "auto_pan_to_selected_dn"],
+            ({ selected_dn, auto_pan_to_selected_dn }) => {
                 if (selected_dn) {
-                    value.activateComponent('universeComponent')
+                    value.activateComponent("universeComponent")
                 }
             }
-        );
+        )
 
-        this.subscribeToSharedState(['rule_editor_selected_rule_id', 'focus_rule_editor' ],
-            ({rule_editor_selected_rule_id, focus_rule_editor}) => {
+        this.subscribeToSharedState(
+            ["rule_editor_selected_rule_id", "focus_rule_editor"],
+            ({ rule_editor_selected_rule_id, focus_rule_editor }) => {
                 if (rule_editor_selected_rule_id && focus_rule_editor) {
-                    value.activateComponent('ruleEditorComponent')
-                    this.sharedState.set('focus_rule_editor', false)
+                    value.activateComponent("ruleEditorComponent")
+                    this.sharedState.set("focus_rule_editor", false)
                 }
-            });
+            }
+        )
     }
 
     closeError() {
-        this.sharedState.set('is_error', false)
-        this.sharedState.set('error', null)
+        this.sharedState.set("is_error", false)
+        this.sharedState.set("error", null)
     }
 
     handleChangeWindow(e) {
         const { windows, layout } = this.state
 
-        const windowId = e.target.getAttribute('tool-window-id');
-        const isVisible = document.getElementById(windowId) !== null;
+        const windowId = e.target.getAttribute("tool-window-id")
+        const isVisible = document.getElementById(windowId) !== null
 
         this.setState({
-            windows: windows.map(component => component.id === windowId ? {
-                ...component,
-                isVisible: isVisible
-            } : component)
+            windows: windows.map((component) =>
+                component.id === windowId
+                    ? {
+                          ...component,
+                          isVisible: isVisible,
+                      }
+                    : component
+            ),
         })
 
         if (isVisible) {
-            layout.hideComponent(windowId);
+            layout.hideComponent(windowId)
         } else {
-            layout.showComponent(windowId);
+            layout.showComponent(windowId)
         }
     }
 
     componentDidMount() {
-        this.subscribeToSharedState(['is_error', 'error'], ({ is_error, error }) => {
-            this.setState({ error: error, isError: is_error})
-        })
+        this.subscribeToSharedState(
+            ["is_error", "error"],
+            ({ is_error, error }) => {
+                this.setState({ error: error, isError: is_error })
+            }
+        )
 
-        this.subscribeToSharedState('popup_window', (popup_window) => {
+        this.subscribeToSharedState("popup_window", (popup_window) => {
             if (popup_window) {
-                this.setState({ 
-                    showPopup: true, 
-                    popupContent: popup_window.content
+                this.setState({
+                    showPopup: true,
+                    popupContent: popup_window.content,
                 })
             } else {
-                this.setState({ 
-                    showPopup: false, 
-                    popupContent: null
+                this.setState({
+                    showPopup: false,
+                    popupContent: null,
                 })
             }
-
         })
     }
 
@@ -131,9 +142,18 @@ class Root extends BaseComponent {
                 <div className="mobile-wrapper">
                     <div className="logo" />
                     <div className="available-msg">
-                        Sorry!<br /><br />
-                        Kubevious works with Desktop browsers only.<br /><br />
-                        <a href="https://kubevious.io/youtube.html" className="link-cta">See Demo in Youtube</a>
+                        Sorry!
+                        <br />
+                        <br />
+                        Kubevious works with Desktop browsers only.
+                        <br />
+                        <br />
+                        <a
+                            href="https://kubevious.io/youtube.html"
+                            className="link-cta"
+                        >
+                            See Demo in Youtube
+                        </a>
                     </div>
                 </div>
                 <div className="wrapper">
@@ -147,9 +167,11 @@ class Root extends BaseComponent {
                         handleLayout={this.handleLayout}
                     />
 
-                    {showPopup && <Popup popupContent={popupContent} /> }
+                    {showPopup && <Popup popupContent={popupContent} />}
 
-                    {isError && <ErrorBox error={error} closeError={this.closeError}/>}
+                    {isError && (
+                        <ErrorBox error={error} closeError={this.closeError} />
+                    )}
                 </div>
             </>
         )
