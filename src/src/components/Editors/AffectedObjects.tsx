ef29660094b0cd677f3c7b0afd13eb5294bd0030
Semyon Pukhov <semyonpuchof@gmail.com>
2021-02-11 11:10:34 +0300

    Transformed to TS AffectedObjects

--- src/src/components/Editors/AffectedObjects.tsx
+++ src/src/components/Editors/AffectedObjects.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { SelectedData } from '../../types'
 import DnShortcutComponent from '../DnShortcutComponent'
+import { SelectedItem } from './types'
 
-const AffectedObjects = ({ selectedItemData }) => {
+const AffectedObjects = ({ selectedItemData }:  { selectedItemData: SelectedItem }): JSX.Element => {
     return (
         <>
-            {selectedItemData.items.map((item, index) => (
+            {selectedItemData.items.map((item: SelectedData, index: number) => (
                 <DnShortcutComponent key={index} dn={item.dn} markers={item.markers} errors={item.errors} warnings={item.warnings}/>
             ))}
         </>
     )
 }
-
 export default AffectedObjects
+
---
+++ src/src/components/Editors/types.ts
@@ -0,0 +1,4 @@
+export type SelectedItem = {
+  status: {},
+  items: []
+}
