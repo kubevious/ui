222968c0991741821691299eba3b39ade7c6d930
Semyon Pukhov <semyonpuchof@gmail.com>
2021-03-09 16:51:27 +0300

    Remove unused function from utils/util

--- src/src/utils/util.ts
+++ src/src/utils/util.ts
@@ -1,5 +1,3 @@
-import { Dn, Alert } from '../types';
-
 export const isEmptyObject = (obj: {}): boolean => {
     for (let key in obj) {
         if (Object.prototype.hasOwnProperty.call(obj, key))
@@ -11,53 +9,3 @@ export const isEmptyObject = (obj: {}): boolean => {
 export const isEmptyArray = (arr: any[]): boolean => { // Later will be corrected on other type
     return !arr || arr.length === 0;
 }
-
-export const getRandomInt = (): number => {
-    return Math.floor(Math.random() * Math.floor(1048576));
-}
-
-export const uniqueMessages = (messages: Alert[]): Alert[] => {
-    let temp: Alert[] = []
-
-    messages?.map(item => {
-        const element = temp.find(tempI => tempI.severity === item.severity && tempI.msg === item.msg)
-
-        if (!element) {
-            temp.push(item)
-        }
-    })
-
-    return temp
-}
-
-export const uniqueObjects = (objects: Dn[]): Dn[] => {
-    let temp: Dn[] = []
-
-    objects?.map(item => {
-        const element = temp.find(tempI => tempI.dn === item.dn)
-
-        if (!element) {
-            temp.push(item)
-        }
-    })
-
-    return temp
-}
-
-export const sortSeverity = (a: Alert, b: Alert): number => {
-    if (a.severity === 'error' && b.severity === 'warn') {
-        return -1
-    }
-
-    if (a.severity === 'warn' && b.severity === 'error') {
-        return 1
-    }
-
-    return 0
-}
-
-export const insertToArray = (arr: any[], index: number, item: any) => [
-    ...arr.slice(0, index),
-    item,
-    ...arr.slice(index)
-]
