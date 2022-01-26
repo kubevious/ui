1ca03954cba0e91058fae731c37647485a638f55
liankar <46632642+liankar@users.noreply.github.com>
2022-01-26 10:12:20 -0800

    Implemented Test Cases (#53)
    * summary test cases, search test cases
    
    * Search area tests
    
    * Search test cases
    
    * Universe Test Cases
    
    * Test cleanup and refactoring

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -22,7 +22,7 @@ import './commands'
 Cypress.Server.defaults({
     delay: 500,
     force404: false,
-    ignore: (xhr) => {
+    whitelist: (xhr) => {
       // handle custom logic for whitelisting
       return true;
     }
