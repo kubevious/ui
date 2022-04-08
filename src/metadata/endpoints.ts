e58c65e5a77027f13cac742fc5d0a3a156046387
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-07 23:21:51 -0700

    updated endpoints.

--- src/metadata/endpoints.ts
+++ src/metadata/endpoints.ts
@@ -5,63 +5,16 @@ export const ENDPOINTS: EndpointInfo[] = [
 
 
     {
-        name: '/project/summary',
+        name: '/rule-engine/rules',
         method: HttpMethod.GET,
     },
     {
-        name: '/project/clusters',
-        method: HttpMethod.GET,
-    },
-    {
-        name: '/project/cluster/config',
-        method: HttpMethod.GET,
-        params: ['cluster'],
-    },
-    {
-        name: '/project/cluster/config',
-        method: HttpMethod.POST,
-        params: ['cluster'],
-        sample: {
-            params: {
-                cluster: '1e5b8f03e71e4a0a8032e468c8424db5',
-            },
-            body: {
-                "name": "cluster-1234",
-                "purpose": [
-                  "Production"
-                ],
-                "location": "us-east1",
-                "provider": "gcp",
-                "description": "This is our test cluster."
-              }
-        },  
-    },
-    {
-        name: '/project/cluster/token',
-        method: HttpMethod.GET,
-        params: ['cluster'],
-    },
-    {
-        name: '/project/cluster/token',
-        method: HttpMethod.POST,
-        params: ['cluster'],
-    },
-    {
-        name: '/project/cluster/reporting_status',
-        method: HttpMethod.GET,
-        params: ['cluster'],
-    },
-    {
-        name: '/project/rule-engine/rules',
-        method: HttpMethod.GET,
-    },
-    {
-        name: '/project/rule-engine/rule',
+        name: '/rule-engine/rule',
         method: HttpMethod.GET,
         params: ['rule'],
     },
     {
-        name: '/project/rule-engine/rule',
+        name: '/rule-engine/rule',
         method: HttpMethod.POST,
         params: ['rule'],
         sample: {
@@ -79,26 +32,26 @@ export const ENDPOINTS: EndpointInfo[] = [
         },
     },
     {
-        name: '/project/rule-engine/rule',
+        name: '/rule-engine/rule',
         method: HttpMethod.DELETE,
         params: ['rule'],
     },
     {
-        name: '/project/rule-engine/rules-statuses',
+        name: '/rule-engine/rules-statuses',
         method: HttpMethod.GET,
     },
     {
-        name: '/project/rule-engine/rule-result',
+        name: '/rule-engine/rule-result',
         method: HttpMethod.GET,
         params: ['rule'],
     },
     {
-        name: '/project/rule-engine/export-rules',
+        name: '/rule-engine/export-rules',
         method: HttpMethod.GET,
         params: [],
     },
     {
-        name: '/project/rule-engine/import-rules',
+        name: '/rule-engine/import-rules',
         method: HttpMethod.POST,
         params: [],
         sample: {
@@ -130,17 +83,17 @@ export const ENDPOINTS: EndpointInfo[] = [
     },
 
     {
-        name: '/project/rule-engine/markers',
+        name: '/rule-engine/markers',
         method: HttpMethod.GET,
         params: [],
     },
     {
-        name: '/project/rule-engine/marker',
+        name: '/rule-engine/marker',
         method: HttpMethod.GET,
         params: ['marker'],
     },
     {
-        name: '/project/rule-engine/marker',
+        name: '/rule-engine/marker',
         method: HttpMethod.POST,
         params: ['marker'],
         sample: {
@@ -156,26 +109,26 @@ export const ENDPOINTS: EndpointInfo[] = [
         },
     },
     {
-        name: '/project/rule-engine/marker',
+        name: '/rule-engine/marker',
         method: HttpMethod.DELETE,
         params: ['marker'],
     },
     {
-        name: '/project/rule-engine/marker-statuses',
+        name: '/rule-engine/marker-statuses',
         method: HttpMethod.GET,
     },
     {
-        name: '/project/rule-engine/marker-result',
+        name: '/rule-engine/marker-result',
         method: HttpMethod.GET,
         params: ['marker'],
     },
     {
-        name: '/project/rule-engine/export-markers',
+        name: '/rule-engine/export-markers',
         method: HttpMethod.GET,
         params: [],
     },
     {
-        name: '/project/rule-engine/import-markers',
+        name: '/rule-engine/import-markers',
         method: HttpMethod.POST,
         params: [],
         sample: {
@@ -197,16 +150,16 @@ export const ENDPOINTS: EndpointInfo[] = [
     },
 
     {
-        name: '/project/validators',
+        name: '/validators',
         method: HttpMethod.GET
     },
     {
-        name: '/project/validators/get',
+        name: '/validators/get',
         method: HttpMethod.GET,
         params: ['validator']
     },
     {
-        name: '/project/validators/set',
+        name: '/validators/set',
         method: HttpMethod.POST,
         sample: {
             body: {
@@ -217,159 +170,127 @@ export const ENDPOINTS: EndpointInfo[] = [
     },
 
     {
-        name: '/project/metrics',
+        name: '/metrics',
         method: HttpMethod.GET
-    },
-    {
-        name: '/project/cluster/metrics',
-        method: HttpMethod.GET,
-        params: ['cluster'],
     },
 
     {
         name: '/cluster/latest_snapshot',
         method: HttpMethod.GET,
-        params: ['cluster'],
     },
     {
         name: '/cluster/snapshots',
         method: HttpMethod.GET,
-        params: ['cluster'],
     },
 
     {
-        name: '/cluster/snapshot/diagram/node',
+        name: '/diagram/node',
         method: HttpMethod.GET,
-        params: ['cluster', 'snapshot', 'dn'],
+        params: ['snapshot', 'dn'],
     },
     {
-        name: '/cluster/snapshot/diagram/children',
+        name: '/diagram/children',
         method: HttpMethod.GET,
-        params: ['cluster', 'snapshot', 'dn'],
+        params: ['snapshot', 'dn'],
     },
     {
-        name: '/cluster/snapshot/diagram/props',
+        name: '/diagram/props',
         method: HttpMethod.GET,
-        params: ['cluster', 'snapshot', 'dn'],
+        params: ['snapshot', 'dn'],
     },
     {
-        name: '/cluster/snapshot/diagram/alerts',
+        name: '/diagram/alerts',
         method: HttpMethod.GET,
-        params: ['cluster', 'snapshot', 'dn'],
+        params: ['snapshot', 'dn'],
     },
     {
-        name: '/cluster/snapshot/diagram/self_alerts',
+        name: '/diagram/self_alerts',
         method: HttpMethod.GET,
-        params: ['cluster', 'snapshot', 'dn'],
+        params: ['snapshot', 'dn'],
+    },
+
+    {
+        name: '/history/timeline_preview',
+        method: HttpMethod.GET,
+    },
+    {
+        name: '/history/timeline',
+        method: HttpMethod.GET,
+        params: ['from', 'to'],
+    },
+    {
+        name: '/history/snapshot_at_date',
+        method: HttpMethod.GET,
+        params: ['date'],
+    },
+    {
+        name: '/history/node',
+        method: HttpMethod.GET,
+        params: ['dn'],
+    },
+    {
+        name: '/history/hierarchy',
+        method: HttpMethod.GET,
+        params: ['dn'],
     },
 
     {
-        name: '/cluster/history/timeline_preview',
+        name: '/history/hierarchy',
         method: HttpMethod.GET,
-        params: ['cluster'],
-    },
-    {
-        name: '/cluster/history/timeline',
-        method: HttpMethod.GET,
-        params: ['cluster', 'from', 'to'],
-    },
-    {
-        name: '/cluster/history/snapshot_at_date',
-        method: HttpMethod.GET,
-        params: ['cluster', 'date'],
+        params: ['dn'],
     },
 
-    // 8b2356d69a614c5b9b83c1f0444eb915
     {
-        name: '/cluster/history/node',
-        method: HttpMethod.GET,
-        params: ['cluster', 'dn'],
-    },
-    {
-        name: '/cluster/history/hierarchy',
-        method: HttpMethod.GET,
-        params: ['cluster', 'dn'],
-    },
-
-
-    {
-        name: '/project/help/request',
+        name: '/search/results',
         method: HttpMethod.POST,
-        params: [],
         sample: {
             body: {
-                request_type: "Problem",
-                subject: "We need help",
-                description: "Have issues with cluster editor",
-                priority: "Medium"
-            },
-        },
-    },
-
-    {
-        name: '/slack/installation',
-        method: HttpMethod.GET,
-        params: [],
-    },
-    {
-        name: '/slack/disconnect',
-        method: HttpMethod.POST,
-        params: [],
-    },
-    {
-        name: '/slack/subscriptions',
-        method: HttpMethod.GET,
-        params: [],
-    },
-    {
-        name: '/slack/delete_subscription',
-        method: HttpMethod.POST,
-        params: [],
-        sample: {
-            body: {
-                channelId: 'C-1234',
-                config: {
-                    type: 'rules'
-                }
-            },
-        },
-    },
-
-    {
-        name: '/project/info',
-        method: HttpMethod.GET,
-    },
-    {
-        name: '/project/team/list',
-        method: HttpMethod.GET,
-    },
-    {
-        name: '/project/team/add',
-        method: HttpMethod.POST,
-        params: [],
-        sample: {
-            body: {
-                email: 'rubenhak@gmail.com',
+                criteria: 'git'
             },
         },
     },
     {
-        name: '/project/team/remove',
+        name: '/search/labels',
         method: HttpMethod.POST,
-        params: [],
         sample: {
             body: {
-                email: 'rubenhak@gmail.com',
+                criteria: 'kubernetes.io'
+            },
+        },
+    },
+    {
+        name: '/search/labels/values',
+        method: HttpMethod.POST,
+        sample: {
+            body: {
+                key: 'kubernetes.io/name',
+                criteria: 'git'
+            },
+        },
+    },
+    {
+        name: '/search/annotations',
+        method: HttpMethod.POST,
+        sample: {
+            body: {
+                criteria: 'kubernetes.io'
+            },
+        },
+    },
+    {
+        name: '/search/annotations/values',
+        method: HttpMethod.POST,
+        sample: {
+            body: {
+                key: 'kubernetes.io/name',
+                criteria: 'git'
             },
         },
     },
 
+
     {
-        name: '/auth/billing/details',
-        method: HttpMethod.GET,
-    },
-    {
-        name: '/auth/billing/portal',
+        name: '/cluster/reporting_status',
         method: HttpMethod.GET,
     },
     
