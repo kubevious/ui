e8e7fc5a3c7388195855fcf39c773d2df0878566
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-18 19:17:49 -0700

    side menu and main tempalte cleanup

--- src/components/MainTemplate/index.tsx
+++ src/components/MainTemplate/index.tsx
@@ -1,121 +1,65 @@
 import React, { FC, useState } from 'react';
-import { subscribeToSharedState } from '@kubevious/ui-framework';
-import { ErrorBox, Popup, OperationLog } from '@kubevious/ui-components';
+import { MainTemplate as BaseMainTemplate } from '@kubevious/ui-components';
 
-import { sharedState } from '../../configureService';
-
-import { ConfirmationDialog } from '@kubevious/ui-components'
-import { HttpClientError } from '@kubevious/http-client'
-
-import { SideMenu } from '../SideMenu';
+import { SideMenuItem, SideMenuSection } from '@kubevious/ui-components';
+import { subscribeToSharedState, useForceUpdate, useSharedState } from '@kubevious/ui-framework';
+import { SIDE_MENU_DATA } from '../../metadata/side-menu';
 
 import styles from './styles.module.css';
 import { SEO } from '../SEO';
 
 export const MainTemplate: FC = ({ children }) => {
 
+    const forceUpdate = useForceUpdate();
+    const [mainSections, setMainSections] = useState<SideMenuSection[]>([]);
     const [isCollapsed, setCollapsed] = useState<boolean>(false);
-    const [error, setError] = useState<Error | null>(null);
-    const [isError, setIsError] = useState<boolean>(true);
 
-    const [showPopup, setShowPopup] = useState<boolean>(true);
-    const [popupContent, setPopupContent] = useState<any>(null);
-
-    subscribeToSharedState(['is_error', 'error'], ({ is_error, error }: { is_error: boolean; error: Error | null }) => {
-        setIsError(is_error);
-        setError(error);
-
-        console.log("[MainTemplate] ERROR: ", error);
-        if (error) {
-            console.log("[MainTemplate] ERROR toString: ", error.toString());
-        }
-    });
-
-    subscribeToSharedState('popup_window', (popup_window) => {
-        if (popup_window) {
-            setShowPopup(true);
-            setPopupContent(popup_window.content);
-        } else {
-            setShowPopup(false);
-            setPopupContent(null);
-        }
-    });
+    const sharedState = useSharedState();
 
     subscribeToSharedState('is_side_menu_collapsed', (value) => {
         setCollapsed(value);
     })
 
-    const closeError = (): void => {
-        sharedState.set('is_error', false);
-        sharedState.set('error', null);
-    };
+    subscribeToSharedState(
+        [
+            'dev_tools_enabled',
+            'notifications_info'
+        ], () => {
+        setMainSections(SIDE_MENU_DATA);
+        forceUpdate();
+    });
 
-    const closePopup = () => {
-        sharedState.set('popup_window', null);
-    };
+    const footerSections : SideMenuItem[] = [];
+    footerSections.push({
+        key: 'close',
+        label: isCollapsed ? 'Expand' : 'Collapse',
+        icon: isCollapsed ? 'open.svg' : 'close.svg',
+        onClick: () => {
+            if (isCollapsed) {
+                sharedState?.set('is_side_menu_collapsed', false);
+            } else {
+                sharedState?.set('is_side_menu_collapsed', true);
+            }
+        },
+    });
 
-    return (<div className={styles.mainContainer}>
-        <SEO />
-        
-        <SideMenu isCollapsed={isCollapsed} />
-
-        <div className={styles.mainComponent}
-             >
+    return (
+    
+        <BaseMainTemplate 
+            firstContent={<SEO />}
+            sideMenuHeader={<>
+                <span className={styles.logoLabel}>OPEN-SOURCE</span>
+                <img src="/img/logoBig.svg" alt="logo" />
+            </>}
+            sideMenuCollapsedHeader={<>
+                <img src="/img/logoSmall.svg" alt="logo" />
+            </>}
+            sideMenuSections={mainSections}
+            sideMenuFooter={footerSections}
+            >
+            
             {children}
-        </div>
-
-        {isError && error && 
-            <ErrorBox error={error}
-                        closeError={closeError}
-                        details={getErrorDetails(error)}
-                        refreshRequest={needsRefreshToRecover(error)}
-                        />
-        }
-
-        {showPopup && <Popup popupContent={popupContent} closePopup={closePopup} />}
-
-        <ConfirmationDialog />
-
-        <OperationLog />
-    </div>
+            
+        </BaseMainTemplate>
     );
 };
-
-
-function getErrorDetails(reason: any)
-{
-    const error = reason as HttpClientError;
-
-    const details : { name: string, value: string }[] = [];
-    if (error.httpUrl) {
-        details.push({ name: 'URL', value: error.httpUrl });
-    }
-    if (error.httpMethod) {
-        details.push({ name: 'Method', value: error.httpMethod });
-    }
-    if (error.httpParams) {
-        details.push({ name: 'Params', value: JSON.stringify(error.httpParams) });
-    }
-    if (error.httpStatusCode) {
-        details.push({ name: 'Status Code', value: error.httpStatusCode.toString() });
-    }
-    if (error.httpStatusText) {
-        details.push({ name: 'Status', value: error.httpStatusText });
-    }
-    return details;
-}
-
-
-function needsRefreshToRecover(reason: any)
-{
-    const error = reason as HttpClientError;
-
-    const statusCode = error?.httpStatusCode;
-    if (statusCode) {
-        if ((statusCode >= 500) && (statusCode < 600)) {
-            return true;
-        }
-    }
-    
-    return false;
-}
