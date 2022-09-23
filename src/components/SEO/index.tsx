9697fd6c11573fa5fad9e32a17b4ce1631499bd9
Kubevious CI/CD <cicd@kubevious.io>
2022-09-23 19:25:16 +0000

    Version Release v1.0.10

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -1,6 +1,34 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import TagManager from "react-gtm-module"
 
 export const SEO = () => {
-    return <Helmet></Helmet>
+    TagManager.initialize({ gtmId: 'GTM-W9ZWCJB' });
+
+    const siteUrl = 'https://demo.kubevious.io/'
+
+    const seo = {
+        description: 'Demo work of kubevious application',
+        title: 'Kubevious :: Demo',
+        keywords: ['kubevious', 'containers', 'docker', 'kubernetes', 'kubernetes dashboard', 'Kubernetes Visualized', 'kubernetes blog', 'kubevious demo'],
+        image: `${siteUrl}img/site-preview.jpg`
+    }
+
+    return <Helmet>
+
+        <meta name="description" content={seo.description} />
+        <meta name="title" content={seo.title} />
+        <meta name="keywords" content={seo.keywords.join(', ')} />
+
+        <meta name="og:image" content={seo.image} />
+        <meta name="og:image:alt" content={seo.title} />
+        <meta name="og:image:width" content="850" />
+        <meta name="og:image:height" content="350" />
+
+        <meta name="og:url" content={siteUrl} />
+        <meta name="og:type" content="website" />
+        <meta name="og:description" content={seo.description} />
+        <meta name="og:title" content={seo.title} />
+
+    </Helmet>
 }
