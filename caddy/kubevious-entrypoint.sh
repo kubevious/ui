9aba0bb8146bb6bf5144c14ba3bd16175888d310
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-09-27 17:22:04 -0700

    fixes to UBI image build.

--- caddy/kubevious-entrypoint.sh
+++ caddy/kubevious-entrypoint.sh
@@ -3,4 +3,8 @@
 echo "Running Kubevious v${KUBEVIOUS_VERSION}"
 echo "window.KUBEVIOUS_VERSION = \"v${KUBEVIOUS_VERSION}\";" > /caddy/www/version.js
 
+echo ">>>> CADDY CONFIG BEGIN"
+cat /etc/caddy/Caddyfile
+echo "<<<< CADDY CONFIG END"
+
 exec "$@"
